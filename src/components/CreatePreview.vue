e375947bd21bf6c966abaae2ffd1378601cb1c4c
Paul Weidner <paul.weidner@gmail.com>
2021-12-27 12:10:23 -0800

    Display the manager user password and additional login info. #8

--- src/components/CreatePreview.vue
+++ src/components/CreatePreview.vue
@@ -2,25 +2,43 @@
   <div>
     <div id="preview-wrapper">
       <h3>{{ this.previewName }}</h3>
-      <template v-if="previewLoginLink">
-        <h3>Your demo of farmOS is ready!</h3>
-        <h4>View the dashboard <a :href="previewLoginLink" target="_blank">here</a> - happy record keeping!</h4>
-      </template>
-      
-      <h4> 
-        <span v-if="previewInfo.state != 'ready'">Status: {{ this.previewInfo.state }}...</span>
-        <span v-if="previewInfo.state == 'ready' && !previewLoginLink">Generating a login link...</span> 
-      </h4> 
-      <div class="progress">
-        <div
-          class="progress-bar progress-bar-striped progress-bar-animated"
-          role="progressbar"
-          aria-valuemin="0"
-          aria-valuemax="100"
-          :aria-valuenow="percentComplete"
-          :style="{width: percentComplete + '%'}"
-        />
+      <div id="preview-building-wrapper" v-if="!previewLoginLink">
+        <h4> 
+          <span v-if="previewInfo.state != 'ready'">Status: {{ this.previewInfo.state }}...</span>
+          <span v-if="previewInfo.state == 'ready'">Generating a login link...</span> 
+        </h4> 
+        <div class="progress">
+          <div
+            class="progress-bar progress-bar-striped progress-bar-animated"
+            role="progressbar"
+            aria-valuemin="0"
+            aria-valuemax="100"
+            :aria-valuenow="percentComplete"
+            :style="{width: percentComplete + '%'}"
+          />
+        </div>
       </div>
+      <div id="preview-login-wrapper" v-if="previewLoginLink">
+        <div>
+          <h4>Your demo of farmOS is ready!</h4>
+          <a :href="previewLoginLink" target="_blank" class="btn btn-outline-primary btn-lg">View the dashboard</a>
+        </div>
+
+        <div>
+          <h5>The button above will log you in to the demo as the "manager" user.</h5>
+          <h5>You may also use the following credentials:</h5>
+          <ul>
+            <li><b>URL:</b> <a :href="previewInfo.url" target="_blank">{{ previewInfo.url }}</a></li>
+            <li><b>Username:</b> manager</li>
+            <li><b>Password:</b> manager</li>
+          </ul>
+        </div>
+
+        <div>
+          <h5>This farmOS demo will persist for <b>1 day</b>.</h5>
+          <h5>Note that sending emails from the demo is not allowed, so <b>password recovery will not work</b>. If you change your user password, be sure to remember it.</h5>
+        </div>
+      </div> 
     </div>
   </div>
 </template>
@@ -165,6 +183,11 @@ export default {
 
 <style>
 
+h3 {
+  margin-top: 2em;
+  margin-bottom: 1em;
+}
+
 h4 {
   margin-bottom: .5em;
 }
@@ -181,5 +204,18 @@ form .radio-wrapper {
 
 input {
     margin: .4rem;
+}
+
+ul {
+  list-style: none;
+}
+
+#preview-wrapper {
+  margin: auto;
+  max-width: 750px;
+}
+
+#preview-login-wrapper div {
+  margin-bottom: 2em;
 }
 </style>
