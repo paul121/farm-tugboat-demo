e14a5a18341263b8aae8a990c78ad2e81be018b8
Paul Weidner <paul.weidner@gmail.com>
2022-01-23 23:06:19 -0800

    Change preview to demo.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="p-3 pb-md-4 mx-auto text-center">
     <h1 class="display-4 fw-normal">Try farmOS</h1>
     <p class="fs-5 mb-0 text-muted">Create a free, private demo of farmOS in 30 seconds.</p>
-    <p class="fs-5 text-muted">Start from scratch or preview farmOS with a curated dataset.</p>
+    <p class="fs-5 text-muted">Start from scratch or demo farmOS with a curated dataset.</p>
   </div>
   <div class="row row-cols-1 row-cols-md-3 mb-3">
     <template v-for="option in basePreviewOptions" :key="option.id">
