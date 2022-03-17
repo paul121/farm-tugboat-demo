4e780ab4b4b138e279fc4e5bbff1007307176bd7
Paul Weidner <paul.weidner@gmail.com>
2022-03-17 10:09:19 -0700

    Retry fetching preview login link. Fixes #15

--- netlify/functions/get-preview-login-link.js
+++ netlify/functions/get-preview-login-link.js
@@ -13,37 +13,44 @@ exports.handler = async function(event, context) {
     }
 
     // Fetch the preview logs.
-    return fetch(`https://api.tugboat.qa/v3/previews/${body.previewId}/log?limit=10`, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${apiKey}`,
-        },
-    })
-    .then(response => response.json())
-    .then(data => {
-        data.reverse();
-        let drushIndex = data.findIndex(log => log.message && log.message.includes('drush uli'));
+    function getLogs(retry) {
+        return fetch(`https://api.tugboat.qa/v3/previews/${body.previewId}/log?limit=10`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${apiKey}`,
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            data.reverse();
+            let drushIndex = data.findIndex(log => log.message && log.message.includes('drush uli'));
 
-        // Bail if no drush uli is found.
-        if (drushIndex < 0) {
+            // If no drush uli is found, retry, then bail if no logs are found.
+            if (drushIndex < 0) {
+                if (retry) {
+                    return new Promise(resolve => setTimeout(resolve, 2000))
+                        .then(getLogs(false));
+                    //return getLogs(false);
+                }
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({message: 'No login link found. Please retry.'}),
+                }
+            } 
+
+            // Return the login link.
+            const body = {loginLink: data[drushIndex -1].message.trim()};
+            return {
+                statusCode: 202,
+                body: JSON.stringify(body),
+            }
+        }).catch(error => {
             return {
                 statusCode: 400,
-                body: 'No login link found. Check the preview status.',
-            }
-        } 
+                body: JSON.stringify({message: error}),
+            };
+        });
+    }
 
-        // Return the login link.
-        const body = {
-            loginLink: data[drushIndex -1].message.trim(),
-        };
-        return {
-            statusCode: 202,
-            body: JSON.stringify(body),
-        }
-    }).catch(error => {
-        return {
-            statusCode: 400,
-            body: error,
-        };
-    });
+    return getLogs(true);
 }
